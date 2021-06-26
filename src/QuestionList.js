28e872f4852c64fec48e55661de01d862b0e622e
Ahmed Dardery <megadardery@yahoo.com>
2021-06-26 23:25:19 +0200

    compilers and update

--- src/QuestionList.js
+++ src/QuestionList.js
@@ -3,26 +3,26 @@ import QuestionBox from './QuestionBox';
 import React, {Component} from 'react';
 import {shuffleArray} from './Utils';
 
-function shuffle(question){
-    if(question.ans != null){
-        return {...question, options: ['True', 'False'], correct: question.ans?0:1, answerIdx:null};
-    }else{
+function shuffle(question) {
+    if (question.ans != null) {
+        return {...question, options: ['True', 'False'], correct: question.ans ? 0 : 1, answerIdx: null};
+    } else {
         const correct = question.correct || 0;
         const rightOne = question.options[correct];
-        let temp = shuffleArray(question.options.filter((v,i)=>i!==correct));
+        let temp = shuffleArray(question.options.filter((v, i) => i !== correct));
         const idx = Math.floor(Math.random() * (question.options.length));
         temp.splice(idx, 0, rightOne);
-        return {...question, options: temp, correct:idx, answerIdx:null};
+        return {...question, options: temp, correct: idx, answerIdx: null};
     }
 }
 
-class QuestionList extends Component{
+class QuestionList extends Component {
     state = {
         data: null
     };
 
     componentDidUpdate(prevProps, prevState) {
-        if(prevProps !== this.props)
+        if (prevProps.questionsData !== this.props.questionsData)
             this.resetAllQuestions();
     }
 
@@ -30,40 +30,50 @@ class QuestionList extends Component{
         this.resetAllQuestions();
     }
 
-    removeCorrect(){
-        this.setState(({data})=>({data: shuffleArray(data.filter(q=>q.answerIdx!==q.correct).map(q=>shuffle(q)))}));
+    removeCorrect() {
+        this.setState(({data}) => ({data: shuffleArray(data.filter(q => q.answerIdx !== q.correct).map(q => shuffle(q)))}));
     }
-    resetAllQuestions(){
+
+    resetAllQuestions() {
         const arr = shuffleArray(this.props.questionsData);
 
-        this.setState({data: arr.map(question=>shuffle(question))});
+        this.setState({data: arr.map(question => shuffle(question))});
     }
-    render(){
-        const onTrigger = (questionIdx, answerIdx)=> {
+
+    render() {
+        const onTrigger = (questionIdx, answerIdx) => {
             console.log(questionIdx, answerIdx);
-            this.setState(({data})=>({data: [
-                    ...data.slice(0,questionIdx),
+            this.setState(({data}) => ({
+                data: [
+                    ...data.slice(0, questionIdx),
                     {
                         ...data[questionIdx],
                         answerIdx
                     },
-                    ...data.slice(questionIdx+1)
-                ]}));
+                    ...data.slice(questionIdx + 1)
+                ]
+            }));
         };
+        const questions = this.state.data && this.state.data.filter(v => v.text.toLowerCase().indexOf(this.props.searchTerm) >= 0);
         return (<section id="questions">
 
             <div className="container">
-                {this.state.data && this.state.data.filter(v=>v.text.toLowerCase().indexOf(this.props.searchTerm)>=0)
-                    .map((v,i)=> <QuestionBox onTrigger={onTrigger} key={i} qidx={i} question={v}/>)}
+                {questions && <div className="question-count">Shown {questions.length} out of {this.props.questionsData.length}</div>}
+                {questions &&
+                questions.map((v, i) => <QuestionBox onTrigger={onTrigger} key={i} qidx={i} question={v}/>)}
                 <div className="container-button">
                     <div>
-                        <button onClick={()=>this.removeCorrect()} className="button correct">Remove Correctly Answered Questions</button>
+                        <button onClick={() => this.removeCorrect()} className="button correct">Remove Correctly
+                            Answered Questions
+                        </button>
                     </div>
                     <div>
-                        <button onClick={()=>this.resetAllQuestions()} className="button reset">Reset all questions</button>
+                        <button onClick={() => this.resetAllQuestions()} className="button reset">Reset all questions
+                        </button>
                     </div>
                 </div>
-            </div></section>);
+            </div>
+        </section>);
 
     }
 }
--- src/QuestionsData/index.js
+++ src/QuestionsData/index.js
@@ -1,11 +1,13 @@
 import dss from "./dss";
 import dssa from "./dssa";
 import modeling from "./modeling";
+import compilers from "./compilers";
 
 const db = {
     DSS: dss,
     DSSA: dssa,
     MODELING: modeling,
+    COMPILERS: compilers,
 }
 
 export default db;
